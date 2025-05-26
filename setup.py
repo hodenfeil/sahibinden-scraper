b604d2fac39cdedb01d9c3e074619c402ac7b410
hodenfeil <hodenfeil@outlook.com>
2025-05-26 22:14:54 +0300

    Update Zyte API key and version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'sahibinden_projesi',
-    version      = '1.3',  # <-- Sürümü güncelledik! Bir sonraki deploy'da 1.4 yapın.
+    version      = '1.4',  # <-- Sürümü güncelledik! Bir sonraki deploy'da 1.4 yapın.
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = sahibinden_projesi.settings']},
 )
